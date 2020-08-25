0ca7aa3492303e05afc7585296884d1deb391ab1
kraplex <kraplex@gmail.com>
2020-08-25 13:00:54 +0300

    hw10

--- hw10 dom/script.js
+++ hw10 dom/script.js
@@ -13,43 +13,36 @@ const wrapper = document.getElementById("wrapper")
 const amountOfWrappersChildren = wrapper.children.length
 const amountOfTagsP = document.querySelectorAll('#wrapper p').length
 const amountOfTagsSpan = document.querySelectorAll('#wrapper span').length
+const amountOftagsWithNumber = isNumber(wrapper)
+const amountOftagsWithString = isString(wrapper)
 
-/*function amountOfTags(tag, parent) {
-    const amount = 0
-    for (i = 0; i < parent.length; i++) {
-        if (parent[i] === "tag") {
-            amount++
-        }
-    } return amount
-}*/
-
-
-console.log()
-
-
-
- function isString(parent) {
+function isString(parent) {
     let sum = 0;
-    for (i = 0; i < parent.children.length; i++) {        
-        if (typeof (Number(parent.children[i].innerText)) !== "number") {
+    for (let i = 0; i < parent.children.length; i++) {
+        let element = +(wrapper.children[i].innerText);
+        if (Number.isNaN(element)) {
             sum = sum + 1
         }
     } return sum
 }
 
-const strings = isString(wrapper)
-console.log(strings) 
-
-const y = Number(wrapper.children[3].innerText)
-console.log(typeof y)
-
-
+function isNumber(parent) {
+    let sum = 0;
+    for (let i = 0; i < parent.children.length; i++) {
+        let element = +(wrapper.children[i].innerText);
+        if (!Number.isNaN(element)) {
+            sum = sum + 1
+        }
+    } return sum
+}
 
 if (knopka) {
     knopka.onclick = function () {
         result.innerText = `
         Общее кол-во всех тегов разных типов внутри wrapper: ${amountOfWrappersChildren};
         Кол-во тегов типа p внутри wrapper: ${amountOfTagsP};
-        Кол-во тегов типа span внутри wrapper: ${amountOfTagsSpan};`
+        Кол-во тегов типа span внутри wrapper: ${amountOfTagsSpan};
+        Кол-во тегов, в которых содержимое - число:  ${amountOftagsWithNumber};
+        Кол-во тегов, в которых содержимое - строка: ${amountOftagsWithString}`
     }
 }
