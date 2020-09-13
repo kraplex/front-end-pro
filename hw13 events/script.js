eaf23717576c444cdc7cba4286c6254da99b9b60
kraplex <kraplex@gmail.com>
2020-09-13 18:24:39 +0300

    2

--- hw13 events/script.js
+++ hw13 events/script.js
@@ -1,5 +1,4 @@
 //1 Дано 5 квадратов, внутри каждого из которых изначально вписана цифра 0. При клике на любой квадрат цифра в нем увеличивается на один.
-//    * сделать чтоб внутри квадратов были кнопки +/- 
 
 const elementsWithCounter = document.querySelectorAll(".wrapper div");
 
@@ -13,19 +12,12 @@ for (let item of elementsWithCounter) {
 
 const elementsWithColors = document.querySelectorAll(".wrapper div");
 
-let itemsClassList = ["blue", "green", "yellow"]
+let itemsClassList = ["blue", "green", "yellow", "orange"]
 
 for (const item of elementsWithColors) {
     item.addEventListener("click", () => {
-        for (let i = 0; i < itemsClassList.length; i++) {
-            if (item.className == itemsClassList[i]) {
-                item.className = itemsClassList[(i + 1)]
-            } else if (item.className == itemsClassList[(itemsClassList.length - 1)]) {
-                item.className = itemsClassList[0]
-            } else {
-                item.className = itemsClassList[(i + 2)]
-            }
-            return item.className
-        }
+        let a = itemsClassList.indexOf(item.className);
+        a == itemsClassList.length - 1 ? item.className = itemsClassList[0] : item.className = itemsClassList[(a + 1)];
     })
 }
+
