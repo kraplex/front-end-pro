c295268b86d3c89e87d1d290ee184adba640e57d
kraplex <kraplex@gmail.com>
2020-09-14 23:14:41 +0300

    hw11

--- hw11 dom/script.js
+++ hw11 dom/script.js
@@ -1,5 +1,48 @@
-document.querySelector('.clickable').addEventListener('click', function(element) {
-    var active = document.querySelector('.clickable .active');
-    if (active) active.classList.remove('active');
-    element.target.classList.add('active');
-  });
+const wrapper = document.querySelector("#wrapper");
+if (wrapper) {
+
+  const formForCreateUser = createFormForCreateUser();
+  wrapper.appendChild(formForCreateUser)
+
+  function createInput(props) {
+    const input = document.createElement("input");
+    const placeholder = props.placeholder || "";
+    const className = props.className || ""
+    input.placeholder = placeholder;
+    input.className = className;
+    return input
+  }
+
+  function createOkButton(props) {
+    const button = document.createElement("button");
+    const buttonText = props.buttonText || "";
+    const className = props.className || "";
+    button.innerText = buttonText;
+    button.className = className;
+    return button
+  }
+
+  function createFormForCreateUser() {
+    const welcomeText = document.createElement("span");
+    welcomeText.innerText = "Enter User Name";
+
+    const FormForCreateUser = document.createElement("div");
+    FormForCreateUser.className = "FormForCreateUser";
+
+    const inputCreateUser = createInput({
+      placeholder: "Enter User Name",
+      className: "inputcreateuser"
+    })
+
+    const okButton = createOkButton({
+      buttonText: "Ok",
+      className: "buttoncreateuser"
+    })
+
+    FormForCreateUser.appendChild(welcomeText)
+    FormForCreateUser.appendChild(inputCreateUser)
+    FormForCreateUser.appendChild(okButton)
+
+    return FormForCreateUser
+  }
+}
