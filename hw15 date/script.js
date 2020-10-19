90d13e630c970c79d882b01ac58fa5c36b37ba97
kraplex <kraplex@gmail.com>
2020-10-19 15:07:50 +0300

    code update

--- hw15 date/script.js
+++ hw15 date/script.js
@@ -7,19 +7,19 @@ wrapper.style.border = "1px solid blue";
 
 
 const select = document.createElement("select");
+
 const option1 = document.createElement("option");
 const option2 = document.createElement("option");
 
-
 select.appendChild(option1);
 select.appendChild(option2);
-
 wrapper.appendChild(select);
+
 option1.innerText = "HH:MM:SS";
 option2.innerText = "HH:MM:SS a.m./p.m.";
 
-option1.value = "one";
-option2.value = "two";
+option1.value = 1;
+option2.value = 2;
 
 
 const div = document.createElement("div");
@@ -30,19 +30,30 @@ renderWatches(dateFormatSimple());
 let intervalIds = [];
 let intervalId = setInterval(() => { renderWatches(dateFormatSimple()) }, 1000);
 intervalIds.push(intervalId);
+console.log(intervalIds);
 
+const arrayOfFunctions = [];
+
+arrayOfFunctions.push(dateFormatSimple);
+arrayOfFunctions.push(dateFormatAmPm);
+
+console.log(arrayOfFunctions);
 
 function dateFormatSimple() {
     const date = new Date();
-    const currentTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    const currentTime = date.getHours() + ":" + minutes + ":" + seconds;
     return currentTime;
 }
 
 function dateFormatAmPm() {
     const date = new Date();
     const hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
     const ampm = date.getHours() >= 12 ? "p.m." : "a.m."
-    const currentTime = hours + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + ampm;
+    const currentTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
     return currentTime;
 }
 
@@ -60,19 +71,28 @@ function renderWatches(string) {
     }
 }
 
-
-
 select.addEventListener("change", (event) => {
+
+    intervalIds.forEach(clearInterval);
+    intervalIds.splice(0,intervalIds.length);
+
     if (event.target.value === "one") {
-        intervalIds.forEach(clearInterval);
+        
         renderWatches(dateFormatSimple());
         let intervalId = setInterval(() => { renderWatches(dateFormatSimple()) }, 1000);
+        
         intervalIds.push(intervalId);
         console.log("one");
+        console.log(intervalIds);
+
+    } else {
         
-    } else intervalIds.forEach(clearInterval);
-    renderWatches(dateFormatAmPm());
-    let intervalId = setInterval(() => { renderWatches(dateFormatAmPm()) }, 1000);
-    intervalIds.push(intervalId);
-    console.log("two");
+        renderWatches(dateFormatAmPm());
+        let intervalId = setInterval(() => { renderWatches(dateFormatAmPm()) }, 1000);
+    
+        intervalIds.push(intervalId);
+        console.log("two");
+        console.log(intervalIds);
+    }
+
 })
