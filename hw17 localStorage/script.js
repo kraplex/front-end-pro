919c0676832969195100c5ec38afb9378fc64048
kraplex <kraplex@gmail.com>
2020-10-26 21:57:12 +0200

    code update

--- hw17 localStorage/script.js
+++ hw17 localStorage/script.js
@@ -1,36 +1,66 @@
 const wrapper = document.querySelector("#wrapper");
+if (wrapper) {
 
-const input = document.createElement("input");
+    const input = document.createElement("input");
 
-const buttonAdd = document.createElement("button");
-buttonAdd.className = "button";
-buttonAdd.innerText = "Add";
+    const buttonAdd = document.createElement("button");
+    buttonAdd.className = "button";
+    buttonAdd.innerText = "Add";
 
-const list = document.createElement("ul");
-
-
-wrapper.appendChild(input);
-wrapper.appendChild(buttonAdd);
-wrapper.appendChild(list);
+    const list = document.createElement("ul");
 
 
-if (localStorage.test) {
+    wrapper.appendChild(input);
+    wrapper.appendChild(buttonAdd);
+    wrapper.appendChild(list);
 
-    const arrayOfData = JSON.parse(localStorage.test);
 
-    for (let i=0; i<arrayOfData.length; i++) {
-        
-        const listItem = document.createElement("li");
-        const span = document.createElement("span");
-        const buttonDelete = document.createElement("button");
-        buttonDelete.innerHTML = "delete";
-        listItem.appendChild(span);
-        listItem.appendChild(buttonDelete);
-        span.innerHTML = arrayOfData[i];
-        list.appendChild(listItem);
-        buttonDelete.addEventListener("click", (e) => {
-            e.target.parentNode.remove();
+    if (localStorage.test) {
+
+        const arrayOfData = JSON.parse(localStorage.test);
+
+        for (let i=0; i<arrayOfData.length; i++) {
             
+            const listItem = document.createElement("li");
+            const span = document.createElement("span");
+            const buttonDelete = document.createElement("button");
+            buttonDelete.innerHTML = "delete";
+            listItem.appendChild(span);
+            listItem.appendChild(buttonDelete);
+            span.innerHTML = arrayOfData[i];
+            list.appendChild(listItem);
+            
+            buttonDelete.addEventListener("click", (e) => {
+                e.target.parentNode.remove();
+                
+                const items = document.querySelectorAll("ul li span");
+                const arr = [];
+                for (let index of items) {
+                    arr.push(index.innerHTML)
+                }
+                const json = JSON.stringify(arr);
+                localStorage.setItem("test", json)
+            });
+                
+        } 
+    }
+
+
+    buttonAdd.addEventListener("click", () => {
+        if (input.value) {
+            
+            const listItem = document.createElement("li");
+            const span = document.createElement("span");
+            const buttonDelete = document.createElement("button");
+            buttonDelete.innerHTML = "delete";
+            listItem.appendChild(span);
+            listItem.appendChild(buttonDelete);
+            span.innerHTML = input.value
+            list.appendChild(listItem)
+            
+            input.value = "";
+            
+            localStorage.clear();
             const items = document.querySelectorAll("ul li span");
             const arr = [];
             for (let index of items) {
@@ -38,48 +68,33 @@ if (localStorage.test) {
             }
             const json = JSON.stringify(arr);
             localStorage.setItem("test", json)
-        });
             
-    } 
+            
+            buttonDelete.addEventListener("click", (e) => {
+                e.target.parentNode.remove();
+                
+                const items = document.querySelectorAll("ul li span");
+                const arr = [];
+                for (let index of items) {
+                    arr.push(index.innerHTML)
+                }
+                const json = JSON.stringify(arr);
+                localStorage.setItem("test", json)
+            });
+        }
+    })
+
+
+
+
+
+
+
 }
 
 
-buttonAdd.addEventListener("click", () => {
-    if (input.value) {
-        
-        const listItem = document.createElement("li");
-        const span = document.createElement("span");
-        const buttonDelete = document.createElement("button");
-        buttonDelete.innerHTML = "delete";
-        listItem.appendChild(span);
-        listItem.appendChild(buttonDelete);
-        span.innerHTML = input.value
-        list.appendChild(listItem)
-        
-        input.value = "";
-        
-        localStorage.clear();
-        const items = document.querySelectorAll("ul li span");
-        const arr = [];
-        for (let index of items) {
-            arr.push(index.innerHTML)
-        }
-        const json = JSON.stringify(arr);
-        localStorage.setItem("test", json)
-        
-         
-        buttonDelete.addEventListener("click", (e) => {
-            e.target.parentNode.remove();
-            
-            const items = document.querySelectorAll("ul li span");
-            const arr = [];
-            for (let index of items) {
-                arr.push(index.innerHTML)
-            }
-            const json = JSON.stringify(arr);
-            localStorage.setItem("test", json)
-        });
-    }
-})
+
+
+
 
 
